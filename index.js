d68bff737c91d25ed32ddd2618c0abe551dc91b3
Jan Mathew <janmathew@MacBookPro.local>
2020-01-06 22:50:17 +0800

    apollo-graphql setup

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+const { ApolloServer } = require('apollo-server-express')
+const { resolvers } = require('./graphql/resolvers')
+const { typeDefs } = require('./graphql/schemas')
 
 const { mongoUri } = require('./keys')
 
 const app = express()
+const server = new ApolloServer({ typeDefs, resolvers })
+server.applyMiddleware({ app })
 
 const PORT = process.env.PORT || 5000
 mongoose
@@ -12,5 +19,9 @@ mongoose
     useUnifiedTopology: true,
     useCreateIndex: true
   })
-  .then(app.listen(PORT))
+  .then(
+    app.listen(PORT, () =>
+      console.log(`GraphQL on port ${PORT}${server.graphqlPath}`)
+    )
+  )
   .catch(error => console.log(error))
