10eb347600c3a5be4b75f419736a9ec1dea9af89
Jan Mathew <janmathew@MacBookPro.local>
2020-01-06 23:10:46 +0800

    added createRecipe mutation, recipeInput

--- graphql/schemas/index.js
+++ graphql/schemas/index.js
@@ -19,8 +19,25 @@ exports.typeDefs = gql`
     favorites: [Recipe]
   }
 
-  type Query {
-    recipes: [Recipe]!
-    users: [User]!
+  input recipeInput {
+    name: String!
+    category: String!
+    description: String!
+    instructions: String!
+    username: String
+  }
+
+  type RootQuery {
+    recipesList: [Recipe]!
+    usersList: [User]!
+  }
+
+  type RootMutation {
+    createRecipe(recipeInput: recipeInput): Recipe
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
   }
 `
