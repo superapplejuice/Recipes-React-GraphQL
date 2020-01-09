6838eefeddbe2cbcf80c2817d13888e1ffceefb0
Jan Mathew <janmathew@MacBookPro.local>
2020-01-09 21:14:51 +0800

    refactor expiresIn to a plain object

--- server/utils/index.js
+++ server/utils/index.js
@@ -3,5 +3,5 @@ const { sign } = require('jsonwebtoken')
 exports.createToken = (user, jwtKey, expiresIn) => {
   const { username, email } = user
 
-  return sign({ username, email }, jwtKey, expiresIn)
+  return sign({ username, email }, jwtKey, { expiresIn })
 }
