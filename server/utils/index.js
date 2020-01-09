8e9e83e0fb636f981a12957a775a7c5ccab42674
Jan Mathew <janmathew@MacBookPro.local>
2020-01-09 21:05:22 +0800

    created util functions

---
+++ server/utils/index.js
@@ -0,0 +1,7 @@
+const { sign } = require('jsonwebtoken')
+
+exports.createToken = (user, jwtKey, expiresIn) => {
+  const { username, email } = user
+
+  return sign({ username, email }, jwtKey, expiresIn)
+}
