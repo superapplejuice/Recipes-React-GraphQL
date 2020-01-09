55ccd6174c9cc2aa34ece234786b8f4ff582448a
Jan Mathew <janmathew@MacBookPro.local>
2020-01-10 00:29:28 +0800

    removed prevailing }

--- client/src/graphql/mutations/index.js
+++ client/src/graphql/mutations/index.js
@@ -1,12 +1,11 @@
 import { gql } from 'apollo-boost'
 
 export const REGISTER_USER = gql`
-  mutation($username: String!, $email: String!, $password: String!) {
+  mutation($username: String!, $email: String, $password: String!) {
     userRegister(
       registerInput: { username: $username, email: $email, password: $password }
     ) {
       token
-      }
     }
   }
 `
