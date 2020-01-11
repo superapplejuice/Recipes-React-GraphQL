ab885c2d233e17fa3b6aa0fcf2f5201ced5cb3a7
Jan Mathew <janmathew@MacBookPro.local>
2020-01-12 01:03:44 +0800

    pass recipe _id to link route

--- client/src/components/recipe/RecipesList.jsx
+++ client/src/components/recipe/RecipesList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const RecipeList = ({ recipes }) => {
   console.log(recipes)
@@ -6,9 +7,11 @@ const RecipeList = ({ recipes }) => {
     <ul>
       {recipes.map(({ _id, name, category, likes }) => (
         <li key={_id}>
-          <div>{name}</div>
-          <div>{category}</div>
-          <div>{likes}</div>
+          <Link to={`/recipe/view/${_id}`}>
+            <div>{name}</div>
+            <div>{category}</div>
+            <div>{likes}</div>
+          </Link>
         </li>
       ))}
     </ul>
