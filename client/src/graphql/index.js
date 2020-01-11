4b20e1775c10a36902988b1b8ad874c3e57fac7e
Jan Mathew <janmathew@MacBookPro.local>
2020-01-11 13:35:53 +0800

    set token to headers

--- client/src/graphql/index.js
+++ client/src/graphql/index.js
@@ -3,5 +3,15 @@ import ApolloClient from 'apollo-boost'
 export const graphqlUri = 'http://localhost:5000/graphql'
 
 export const client = new ApolloClient({
-  uri: graphqlUri
+  uri: graphqlUri,
+  fetchOptions: { credentials: 'include' },
+  request: operation => {
+    const token = localStorage.getItem('token')
+    operation.setContext({
+      headers: { authorization: token }
+    })
+  },
+  onError: errors => {
+    errors && console.log(errors)
+  }
 })
