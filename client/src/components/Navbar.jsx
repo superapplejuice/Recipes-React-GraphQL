1272dac1a0da76724373e3355dc521d0273d99b8
Jan Mathew <janmathew@MacBookPro.local>
2020-01-11 23:50:14 +0800

    handling pseudo auth status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+  const auth = false
+
+  const handleAuth = () => {
+    return auth ? (
+      <Fragment>
+        <NavLink to='/recipe/add'>Add Recipe</NavLink>
+        <NavLink to='/profile'>Profile</NavLink>
+        <button>Logout</button>
+      </Fragment>
+    ) : (
+      <Fragment>
+        <NavLink to='/auth/login'>Login</NavLink>
+        <NavLink to='/auth/register'>Register</NavLink>
+      </Fragment>
+    )
+  }
+
   return (
     <nav>
       <NavLink exact to='/'>
         Home
       </NavLink>
-      <br />
       <NavLink to='/recipe/search'>Search</NavLink>
-      <br />
-      <NavLink to='/auth/login'>Login</NavLink>
-      <br />
-      <NavLink to='/auth/register'>Register</NavLink>
-      <br />
+      {handleAuth()}
     </nav>
   )
 }
