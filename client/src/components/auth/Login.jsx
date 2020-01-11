eb808c0ba745c376b0bc2fc760b041b7a1de16d4
Jan Mathew <janmathew@MacBookPro.local>
2020-01-11 12:47:53 +0800

    added login functionality

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import * as yup from 'yup'
+
+import { LOGIN_USER } from '../../graphql/mutations'
+
+import AuthForm from '../../utils/components/AuthForm'
+import FormField from '../../utils/components/FormField'
 
 const Login = () => {
+  const formTitle = 'Login'
+  const successMessage = 'Login successful!'
+
+  const initialValues = {
+    email: '',
+    password: ''
+  }
+
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email('Please enter a valid email')
+      .required('Please enter an email'),
+    password: yup.string().required('Please enter your password')
+  })
+
   return (
-    <div>
-      <div>Login</div>
-    </div>
+    <AuthForm
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      mutation={LOGIN_USER}
+      formTitle={formTitle}
+      successMessage={successMessage}
+    >
+      <div>
+        <FormField name='email' type='email' label='Email' />
+      </div>
+      <div>
+        <FormField name='password' type='password' label='Password' />
+      </div>
+    </AuthForm>
   )
 }
 
