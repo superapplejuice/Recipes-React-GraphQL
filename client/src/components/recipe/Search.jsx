5a193cd1d96c970de5a7978fb15a620c2b94199b
Jan Mathew <janmathew@MacBookPro.local>
2020-01-16 21:26:57 +0800

    added search function

--- client/src/components/recipe/Search.jsx
+++ client/src/components/recipe/Search.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect, createRef, Fragment } from 'react'
+import { useQuery } from 'react-apollo'
+
+import { SEARCH_RECIPES } from '../../graphql/queries'
+import RecipeList from './RecipesList'
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const { data, loading, error } = useQuery(SEARCH_RECIPES, {
+    variables: { searchTerm }
+  })
+  const inputRef = createRef()
+  console.log()
+
+  useEffect(() => {
+    inputRef.current.focus()
+    // eslint-disable-next-line
+  }, [])
+
+  const renderRecipes = () => {
+    return !error && loading ? (
+      <p>Loading...</p>
+    ) : error && !loading ? (
+      <p>Error while searching for recipes!</p>
+    ) : data.searchRecipe.length > 0 ? (
+      <Fragment>
+        <p>List of Recipes</p>
+        <RecipeList recipes={data.searchRecipe} />
+      </Fragment>
+    ) : (
+      <p>No recipes found!</p>
+    )
+  }
+
   return (
     <div>
-      <div>Search</div>
+      <label>Search for a Recipe</label>
+      <input
+        type='text'
+        ref={inputRef}
+        placeholder='Recipe'
+        onChange={event => setSearchTerm(event.target.value)}
+        value={searchTerm}
+      />
+      <div>{renderRecipes()}</div>
     </div>
   )
 }
