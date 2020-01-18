1b284eb13d4a6ad85b4e7ec24051b958e5ad3134
Jan Mathew <janmathew@MacBookPro.local>
2020-01-18 11:42:46 +0800

    added condition to render delete button

--- client/src/utils/components/RecipesList.jsx
+++ client/src/utils/components/RecipesList.jsx
@@ -1,20 +1,29 @@
 import React from 'react'
+import { useQuery } from 'react-apollo'
 import { Link } from 'react-router-dom'
 import { array } from 'prop-types'
 
-const RecipesList = ({ recipes }) => (
-  <ul>
-    {recipes.map(({ _id, name, category, likes }) => (
-      <li key={_id}>
-        <Link to={`/recipe/view/${_id}`}>
-          <div>{name}</div>
-          <div>{category}</div>
-          <div>{likes} likes</div>
-        </Link>
-      </li>
-    ))}
-  </ul>
-)
+import { GET_CURRENT_USER } from '../../graphql/queries'
+
+const RecipesList = ({ recipes }) => {
+  const { data } = useQuery(GET_CURRENT_USER)
+
+  return (
+    <ul>
+      {recipes.map(({ _id, name, category, likes, username }) => (
+        <li key={_id}>
+          <Link to={`/recipe/view/${_id}`}>
+            <div>{name}</div>
+            <div>{category}</div>
+            <div>{likes} likes</div>
+          </Link>
+          {data.currentUser !== null &&
+            username === data.currentUser.username && <button>Delete</button>}
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 RecipesList.propTypes = {
   recipes: array.isRequired
