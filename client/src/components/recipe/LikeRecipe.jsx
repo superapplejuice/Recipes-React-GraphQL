4fdc8765ff6622ba684e4a91a5610ada11e9f082
Jan Mathew <janmathew@MacBookPro.local>
2020-01-19 22:19:11 +0800

    changed to class, added like/unlike

--- client/src/components/recipe/LikeRecipe.jsx
+++ client/src/components/recipe/LikeRecipe.jsx
@@ -1,29 +1,82 @@
-import React, { useState, useEffect } from 'react'
-import { useMutation } from 'react-apollo'
+import React, { PureComponent } from 'react'
+import { Mutation } from 'react-apollo'
 
 import { LIKE_RECIPE } from '../../graphql/mutations'
-
+import { FETCH_RECIPE } from '../../graphql/queries'
 import withSession from '../../utils/functions/withSession'
 
-const LikeRecipe = ({ session: { currentUser }, _id }) => {
-  const [username, setUsername] = useState(null)
-  const [likeRecipe, { loading }] = useMutation(LIKE_RECIPE)
+class LikeRecipe extends PureComponent {
+  state = {
+    username: null,
+    liked: false
+  }
 
-  useEffect(() => {
-    currentUser && setUsername(currentUser.username)
-  }, [currentUser])
+  componentDidMount() {
+    const {
+      session: { currentUser },
+      _id
+    } = this.props
+    // returns an element that matches the condition
+    // returns -1 (false) || 0 (pass)
+    const prevLiked =
+      currentUser.favourites.findIndex(item => item._id === _id) > -1
 
-  return (
-    username && (
-      <button
-        onClick={() => {
-          likeRecipe({ variables: { _id, username } })
+    return (
+      currentUser &&
+      this.setState({ username: currentUser.username, liked: prevLiked })
+    )
+  }
+
+  handleLike = likeRecipe => {
+    const { refetch } = this.props
+    const { liked } = this.state
+
+    liked
+      ? likeRecipe().then(async ({ data }) => {
+          console.log(data)
+          return await refetch()
+        })
+      : console.log('unlike')
+  }
+
+  handleClick = likeRecipe => {
+    this.setState(
+      prevState => ({ liked: !prevState.liked }),
+      () => this.handleLike(likeRecipe)
+    )
+  }
+
+  render() {
+    const { _id } = this.props
+    const { username, liked } = this.state
+
+    return (
+      <Mutation
+        mutation={LIKE_RECIPE}
+        variables={{ _id, username }}
+        update={(cache, { data: { likeRecipe } }) => {
+          const { getRecipe } = cache.readQuery({
+            query: FETCH_RECIPE,
+            variables: { _id }
+          })
+
+          return cache.writeQuery({
+            query: FETCH_RECIPE,
+            variables: _id,
+            data: { getRecipe: { ...getRecipe, likes: likeRecipe.likes + 1 } }
+          })
         }}
       >
-        {loading ? 'Loading...' : 'Like'}
-      </button>
+        {likeRecipe =>
+          username && (
+            <button onClick={() => this.handleClick(likeRecipe)}>
+              {liked ? 'Unlike' : 'Like'}
+            </button>
+          )
+        }
+      </Mutation>
     )
-  )
+  }
 }
 
 export default withSession(LikeRecipe)
